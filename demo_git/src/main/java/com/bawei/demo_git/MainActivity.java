c620bef701fd941ac6fc67397e70ce23b9908155
kjl-12 <2038535761@qq.com>
2019-11-29 14:52:54 +0800

    Initial commit

---
+++ demo_git/src/main/java/com/bawei/demo_git/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bawei.demo_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
